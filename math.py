f256bc65e2cc7edaf814970b617660b0604a6e57
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-11 23:05:35 -0500

    Update math.py

--- math.py
+++ math.py
@@ -1,11 +1,19 @@
-#david = 26 #this is variable 1
-#ben = 26 #this is variable 2
-#print(david*ben) #this is the output
+"""
+david = 26 #this is var1
+ben = 26 #this is var2
+print(david*ben) #this is the output
 x = 26 #interger variable
 y = "ben" #string variable
-
 print(x)
 print(y)
 """
-this is a comment section used for testing only
+
 """
+this is a comment section used for testing only
+lower case and upercase are two different variables
+i can use numbers too but i cant use hash marks
+"""
+
+x, y, z = '1', '2', '3'
+
+print(x,y,z)
