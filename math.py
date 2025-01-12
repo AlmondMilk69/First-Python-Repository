e1f6b606b442f7da4a6166c045e1023d558fcf80
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-11 19:23:44 -0500

    Update math.py

--- math.py
+++ math.py
@@ -1,6 +1,11 @@
-david = 26 #this is variable 1
-ben = 26 #this is variable 2
-print(david*ben) #this is the output
+#david = 26 #this is variable 1
+#ben = 26 #this is variable 2
+#print(david*ben) #this is the output
+x = 26
+y = "ben"
+
+print(x)
+print(y)
 """
 this is a comment section used for testing only
 """
