51e465071de10e6d216dd46ecc61f8f6dab0d84e
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-11 19:25:51 -0500

    Update math.py

--- math.py
+++ math.py
@@ -1,8 +1,8 @@
 #david = 26 #this is variable 1
 #ben = 26 #this is variable 2
 #print(david*ben) #this is the output
-x = 26
-y = "ben"
+x = 26 #interger variable
+y = "ben" #string variable
 
 print(x)
 print(y)
