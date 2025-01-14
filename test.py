18f0a9109624f745e824c85a7ebcd1c79a91e127
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-14 17:44:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,12 @@ age = 2025 - int(birth_year)
 print(age)
 '''
 
-x = input('What is the value of x? ')
-y = input('What is the value of y? ')
-a = input('What operation would you like to perform? ')
-z = int(x) str(a) int(y)
+x = input('What is the value of x? ').strip
+y = input('What is the value of y? ').strip
+# i dont understand why .strip is throwing an error???
+# working on how to ask the user what operation they want to perform
+# a = input('What operation would you like to perform? ')
+
+z = int(x) + int(y)
 
 print('z is equal to:', int(z))
