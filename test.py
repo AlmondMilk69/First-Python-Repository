fded2de871129ef8d14e8758b62728a7f29e3582
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-14 17:38:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ print(age)
 
 x = input('What is the value of x? ')
 y = input('What is the value of y? ')
-
-z = int(x) + int(y)
+a = input('What operation would you like to perform? ')
+z = int(x) str(a) int(y)
 
 print('z is equal to:', int(z))
