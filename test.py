bbb5c4ffd83ce94d318b16be9b7d5bd704428763
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-14 17:34:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,9 @@ age = 2025 - int(birth_year)
 print(age)
 '''
 
+x = input('What is the value of x? ')
+y = input('What is the value of y? ')
+
+z = int(x) + int(y)
+
+print('z is equal to:', int(z))
