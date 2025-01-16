b0a1af3a0b7b076252d7a5f33a57233d57714893
AlmondMilk69 <benjamin.utberg98@gmail.com>
2025-01-15 21:42:22 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ age = 2025 - int(birth_year)
 print(age)
 '''
 
-x = int(input('What is the value of x? '))
-y = int(input('What is the value of y? '))
+x = float(input('What is the value of x? '))
+y = float(input('What is the value of y? '))
 # working on how to ask the user what operation they want to perform
 # a = input('What operation would you like to perform? ')
 
